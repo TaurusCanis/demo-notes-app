4b2f8ab9c2601fc7b0cb0cb5a0cefb33401948a0
Taurus Canis <acdole09@gmail.com>
2021-09-21 15:23:01 -0400

    fixed routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Notes from "./containers/Notes";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
+import NotFound from "./containers/NotFound";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
       <UnauthenticatedRoute exact path="/login">
         <Login />
       </UnauthenticatedRoute>
@@ -28,9 +31,7 @@ export default function Routes() {
       <AuthenticatedRoute exact path="/notes/:id">
         <Notes />
       </AuthenticatedRoute>
-      <UnauthenticatedRoute exact path="/">
-        <Home />
-      </UnauthenticatedRoute>
+      {/* Finally, catch all unmatched routes */}
       <Route>
         <NotFound />
       </Route>
